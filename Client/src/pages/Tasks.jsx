d908f6d6d13b80896ad2bcbff0391fbd30f9ad8c
Vishwaksen0124 <vishwaksenpujala@gmail.com>
2025-05-05 07:12:44 +0530

    change

--- Client/src/pages/Tasks.jsx
+++ Client/src/pages/Tasks.jsx
@@ -10,7 +10,7 @@ import Tabs from "../components/Tabs";
 import TaskTitle from "../components/TaskTitle";
 import BoardView from "../components/BoardView";
 import Table from "../components/Task/Table";
-import AddTask from "../components/task/AddTask";
+import AddTask from "../components/Task/AddTask";
 import { useGetAllTaskQuery } from "../redux/slices/api/taskApiSlice";
 import { useSelector } from "react-redux";
 
