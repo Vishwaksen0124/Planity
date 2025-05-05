c28ddb62ffcf7c496b26d0954b7beb838b9e2786
Vishwaksen0124 <vishwaksenpujala@gmail.com>
2025-05-05 11:52:04 +0530

    nginx

--- Client/src/redux/slices/apiSlice.js
+++ Client/src/redux/slices/apiSlice.js
@@ -1,7 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const isLocal = window.location.hostname === "localhost";
-const API_URI = isLocal ? "http://localhost:8800" : window.location.origin;
+const API_URI = isLocal
+  ? "http://localhost:8800"
+  : "https://planity-4l0m.onrender.com";
+
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI +"/api" });
 
