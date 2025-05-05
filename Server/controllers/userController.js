7ff3e1f57a09dfd7678487951e82f048295adffc
Vishwaksen0124 <vishwaksenpujala@gmail.com>
2025-05-05 13:10:44 +0530

    user

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -84,9 +84,11 @@ export const logoutUser = async (req, res) => {
   try {
     res.cookie("token", "", {
       httpOnly: true,
-      expires: new Date(0),
+      secure: true,
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-    
+
     res.status(200).json({ message: "Logout successful" });
   } catch (error) {
     console.log(error);
